9bcc53de172c37d1fe9ceb71b020d0535e4002a0
Igor <martynov.igor2020ty@gmail.com>
2024-11-23 02:20:11 +0300

    Add admin panel for Celestial and Category models
    - Configured display and editing fields in the admin for the Celestial model.
    - Added inline for images related to Celestial.
    - Set up filters and pagination for easier record management.
    - Configured automatic slug generation for Celestial and Category models.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Celestial, Category, ImageCelestial
 
-# Register your models here.
+
+class ImageCelestialInline(admin.TabularInline):
+    model = ImageCelestial
+    extra = 3
+
+
+@admin.register(Celestial)
+class CelestialAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated',
+                    'discount']
+    list_editable = ['price', 'available', 'discount']
+    prepopulated_fields = {'slug': ('name',)}
+    list_display_links = ['name', 'slug']
+    list_filter = ['available', 'created', 'updated']
+    list_per_page = 5
+    inlines = [ImageCelestialInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
