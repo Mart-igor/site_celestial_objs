b182478a894f2d91308514c89d1f33a4b8b4b6aa
Igor <martynov.igor2020ty@gmail.com>
2024-11-23 20:34:42 +0300

    Added pagination and links to downloadable files
    Added pagination to category_list

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'main'
 
@@ -9,3 +11,7 @@ urlpatterns = [
     path('shop/category/<slug:category_slug>/', views.category_list, name='category_list'),
     path('shop/<slug:detail_slug>/', views.detail, name='detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEdiA_URL,
+                          document_root=settings.MEDIA_ROOT)
